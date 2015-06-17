0fac50e69ec1a8f31002bad405efd90c67272d13
autodefrost <galdebert@gmail.com>
2015-06-17 22:17:09 +0200

    cpp-sandbox

---
+++ cpp-sandbox/src/main.cpp
@@ -0,0 +1,27 @@
+#include "concepts.h"
+
+void Lambdas()
+{
+
+}
+
+// auto
+// decltype
+// using
+// nullptr
+// Range - based for loops
+// Override and final
+// Strongly - typed enums aka scoped enums
+// Smart pointers
+// Lambdas
+// non-member begin() and end()
+// static_assert
+// type traits
+// Move semantics
+
+// 
+
+int main()
+{
+	return 0;
+}
