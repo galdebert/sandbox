ed43fa82b8091dfb131d3070cedf457e25a3f3ad
autodefrost <galdebert@gmail.com>
2015-09-10 13:31:15 +0200

    some python

---
+++ run-clang-format/format-google.py
@@ -0,0 +1,5 @@
+import run_clang_format
+run_clang_format.run(
+	[r"C:\Users\galde_000\Dev\sandbox\cpp-sandbox\src",
+	"--style", "Google"])
+
---
+++ run-clang-format/format-llvm.py
@@ -0,0 +1,3 @@
+import run_clang_format
+run_clang_format.run([r"C:\Users\galde_000\Dev\sandbox\cpp-sandbox\src", "--style", "LLVM"])
+input("PRESS ENTER TO CONTINUE.")
