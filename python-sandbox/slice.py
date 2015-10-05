4dc0723b1a80bb10322b23f54f3027392920be4a
autodefrost <galdebert@gmail.com>
2015-10-06 00:49:16 +0200

    slice.py

---
+++ python-sandbox/slice.py
@@ -0,0 +1,15 @@
+from unittest import TestCase, main
+
+
+class SliceTestCase(TestCase):
+
+    def test(self):
+        a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        b = a[:2]
+        print("a[:2]={}".format(a[:2]))
+        print("a[-2:]={}".format(a[-2:]))
+        print("a[2:-2]={}".format(a[2:-2]))
+        print("a[0:]={}".format(a[0:]))
+
+if __name__ == '__main__':
+    main()
