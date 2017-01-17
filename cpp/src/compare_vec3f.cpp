90f2f8c767a2d6904dc417b79c7709f3ed5c4cd9
Guillaume Aldebert <guillaume.aldebert@autodesk.com>
2017-01-17 12:30:29 +0100

    better compare vec3f

--- cpp/src/compare_vec3f.cpp
+++ cpp/src/compare_vec3f.cpp
@@ -44,25 +44,24 @@ __forceinline void ProfileSort(const std::string& title, F less, std::vector<Vec
   std::cout << seconds << '\n';
 }
 
+struct CmpIO {
+  Vec3f A;
+  Vec3f B;
+  bool r;
+};
 
-template<typename F>
-void ProfileCmp(const std::string& title, F less, std::vector<Vec3f>& arr)
-{
+template <typename F>
+void ProfileCmp(const std::string &title, F less, std::vector<CmpIO> &ios) {
   std::cout << title << " = ";
 
-  auto start = std::chrono::steady_clock::now();
+  auto start = std::chrono::high_resolution_clock::now();
 
-  bool result = false;
-  for (int i = 0; i < arr.size() / 2; ++i)
-  {
-	bool r = less(arr[i*2], arr[i*2+1]);
-    result = result ^ r;
-  }
+  for (auto& io : ios) { io.r = less(io.A, io.B); }
 
-  auto finish = std::chrono::steady_clock::now();
+  auto finish = std::chrono::high_resolution_clock::now();
 
   auto seconds = std::chrono::duration_cast<std::chrono::duration<double>>(finish - start).count();
-  std::cout << result << " in "<< seconds << '\n';
+  std::cout << seconds << '\n';
 }
 
 template <typename F>
@@ -76,25 +75,38 @@ void TestCompareVec3f() {
 
   const float extent = 1000.0f;
   const int count = 1000'000;
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_real_distribution<float> dis(-extent, extent);
-
-  std::vector<Vec3f> arr(count);
-  for (auto &v : arr) {
-    v.x = dis(gen);
-    v.y = dis(gen);
-    v.z = dis(gen);
-  }
+  std::mt19937 gen;
+  std::uniform_real_distribution<float> distrib(-extent, extent);
 
   const int nb = 5;
+
+  std::vector<Vec3f> arr(count);
+  for (auto& v : arr) {
+    v.x = distrib(gen);
+    v.y = distrib(gen);
+    v.z = distrib(gen);
+  }
   Repeat(nb, [&a=arr]() { ProfileSort("sort less_vec3f_1", less_vec3f_1, a); });
   Repeat(nb, [&a=arr]() { ProfileSort("sort less_vec3f_2", less_vec3f_2, a); });
   Repeat(nb, [&a=arr]() { ProfileSort("sort less_vec3f_3", less_vec3f_3, a); });
 
-  Repeat(nb, [&a=arr]() { ProfileCmp("cmp less_vec3f_1", less_vec3f_1, a); });
-  Repeat(nb, [&a=arr]() { ProfileCmp("cmp less_vec3f_2", less_vec3f_2, a); });
-  Repeat(nb, [&a=arr]() { ProfileCmp("cmp less_vec3f_3", less_vec3f_3, a); });
+  std::vector<CmpIO> ios(count);
+  for (auto& io : ios) {
+    io.A.x = distrib(gen);
+    io.A.y = distrib(gen);
+    io.A.z = distrib(gen);
+    io.B.x = distrib(gen);
+    io.B.y = distrib(gen);
+    io.B.z = distrib(gen);
+	io.r = false;
+  }
+  Repeat(nb, [&ios=ios]() { ProfileCmp("compute less_vec3f_1", less_vec3f_1, ios); });
+  Repeat(nb, [&ios=ios]() { ProfileCmp("compute less_vec3f_2", less_vec3f_2, ios); });
+  Repeat(nb, [&ios=ios]() { ProfileCmp("compute less_vec3f_3", less_vec3f_3, ios); });
 
+  auto count_true =
+      std::count_if(ios.begin(), ios.end(), [](auto &x) { return x.r; });
+
+  std::cout << "count_true = " << count_true << '\n';
   std::cout << "\n\n";
 }
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -16,7 +16,7 @@ int main() {
   //TestLambda();
   //TestMove();
   TestCompareVec3f();
-  TestDivNearest();
+  //TestDivNearest();
 
   getchar();
   return 0;
