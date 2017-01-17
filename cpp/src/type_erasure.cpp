ca783d11c5829fc5bd59935c3da596284475f4cd
Guillaume Aldebert <guillaume.aldebert@autodesk.com>
2017-01-17 14:04:56 +0100

    better comments in type_erasure

--- cpp/src/type_erasure.cpp
+++ cpp/src/type_erasure.cpp
@@ -6,37 +6,42 @@ namespace type_erasure {
 
 using namespace std;
 
+// The idea
+// Object (without virtual func) can store any Data type (without virtual func)
+// Object::Func() will call Func(const Data&)
+
+// How:
+// Object has a ptr to Object::Interface
+// Object::Interface has virtual function DoFunc()
+// Object::Concrete<T> inherit from Object::Interface
+// Object::Concrete<T>::DoFunc() call Func(const T&)
+// Object::Object<T> constructor create a Object::Concrete<T> on the heap
+// In less object oriented terms, we simply prepend a vtableptr to the Data object: Object = ptr to [vtableptr][Data]
 class Object {
- public:
+public:
   void DoFunc() { interface_->DoFunc(); }
 
-  template <typename T>
-  Object(const T& data) {
+  template <typename T> Object(const T &data) {
     interface_ = make_shared<Concrete<T>>(data);
   }
 
-  // alt:
-  // template <typename T>
-  // Object(T data) {
-  //  interface_ = make_shared<Concrete<T>>(move(data));
-  //}
+  Object(const Object &other) { interface_ = other.interface_->Clone(); }
 
-  Object(const Object& other) { interface_ = other.interface_->Clone(); }
-
- private:
+private:
   struct Interface {
     virtual ~Interface() {}
     virtual shared_ptr<Interface> Clone() const = 0;
     virtual void DoFunc() = 0;
   };
 
-  template <typename T>
-  struct Concrete : public Interface {
-    Concrete(const T& data) : data_(data) {}
-    // alt: Concrete(T data) : data_(move(data)) {}
+  template <typename T> struct Concrete : public Interface {
+
+    Concrete(const T &data) : data_(data) {}
+
     virtual shared_ptr<Interface> Clone() const override {
       return make_shared<Concrete<T>>(data_);
     }
+
     virtual void DoFunc() override { Func(data_); }
     T data_;
   };
@@ -44,7 +49,8 @@ class Object {
   shared_ptr<Interface> interface_;
 };
 
-
+// Func(const A&), Func(const B&), Func(int)
+// note that struct A, B have no virtual functions
 struct A {
   A() { value_ = 2.0f; }
   float value_;
@@ -66,6 +72,8 @@ struct B {
 void Func(const B& b) { cout << "Do B " << b.values_.size() << "\n"; }
 
 void Func(int i) { cout << "Do int " << i << "\n"; }
+
+
 
 void Test() {
   vector<Object> objects;
