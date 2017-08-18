11b3c7f39e73c2e31613ec3bed63b627b1bd3003
Guillaume Aldebert <guillaume.aldebert@autodesk.com>
2017-08-18 18:30:25 +0200

    copy_headers

---
+++ python/copy_headers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+
+from pathlib import Path
+from distutils.file_util import copy_file
+
+def copy_headers(src_dir, dst_dir):
+    for src in Path(src_dir).glob('**/*.h'):
+        dst = Path(dst_dir).joinpath(src.relative_to(src_dir))
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        copy_file(src, dst)
