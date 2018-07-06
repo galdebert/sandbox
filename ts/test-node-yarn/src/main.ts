c4b83d8fc4c41a26ffb5148eb9315936a1967d55
Guillaume Aldebert <galdebert@gmail.com>
2018-07-06 18:12:18 +0200

    rm test-node-yarn

--- ts/test-node-yarn/src/main.ts
+++
@@ -1,8 +0,0 @@
-// import { ZipCodeValidator } from "./ZipCodeValidator";           Import a single export from a module
-// import { ZipCodeValidator as ZCV } from "./ZipCodeValidator";    Imports can also be renamed
-// import * as validator from "./ZipCodeValidator";                 Import the entire module into a single variable, and use it to access the module exports
-// import "./my-module.js";                                         Import a module for side-effects only
-
-import * as test_fs from "./test_fs";
-
-test_fs.test_fs();
